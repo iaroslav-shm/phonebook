6e3e6a6a5b687571b9bac1c3a22e57ae7864a554
Iaroslav Shmorgunov <yaroslav.shm@gmail.com>
2022-09-04 20:27:17 +0300

    Created PhoneBook add Lombok

---
+++ src/test/java/org/shmorgunov/phonebook/domain/PhoneBook.java
@@ -0,0 +1,25 @@
+package org.shmorgunov.phonebook.domain;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class PhoneBook {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String phone;
+    private LocalDateTime lastModified = LocalDateTime.now();
+}
