b8b4ee70409d5bc6eaf62c288cda20557349be05
Iaroslav Shmorgunov <yaroslav.shm@gmail.com>
2022-09-04 20:07:05 +0300

    Initial commit

---
+++ src/test/java/org/shmorgunov/phonebook/PhonebookApplicationTests.java
@@ -0,0 +1,13 @@
+package org.shmorgunov.phonebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhonebookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
