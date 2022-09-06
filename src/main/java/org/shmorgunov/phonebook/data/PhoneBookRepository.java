a2979f9ddc7ef068a3283266603cdc94dc1f3e0d
Iaroslav Shmorgunov <yaroslav.shm@gmail.com>
2022-09-06 13:10:04 +0300

    Change CrudRepository to JpaRepository

--- src/main/java/org/shmorgunov/phonebook/data/PhoneBookRepository.java
+++ src/main/java/org/shmorgunov/phonebook/data/PhoneBookRepository.java
@@ -1,10 +1,10 @@
 package org.shmorgunov.phonebook.data;
 
 import org.shmorgunov.phonebook.domain.PhoneRecord;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface PhoneBookRepository extends CrudRepository<PhoneRecord, Long> {
+public interface PhoneBookRepository extends JpaRepository<PhoneRecord, Long> {
 }
