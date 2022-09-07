cac2d1c12855c449fa42a198bab48a8391a8a338
Iaroslav Shmorgunov <yaroslav.shm@gmail.com>
2022-09-07 14:08:52 +0300

    Add Validation to PhoneRecord
    It can check:
    - That name is not null and size
    - That phone is not null, and pattern with numbers

--- src/main/java/org/shmorgunov/phonebook/domain/PhoneRecord.java
+++ src/main/java/org/shmorgunov/phonebook/domain/PhoneRecord.java
@@ -1,25 +1,44 @@
 package org.shmorgunov.phonebook.domain;
 
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 
+import static org.shmorgunov.phonebook.util.StringModificationHelper.formatPhoneNumber;
+import static org.shmorgunov.phonebook.util.StringModificationHelper.trimName;
+
 @Data
-@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @Entity
 public class PhoneRecord {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull(message = "Name must be not null")
+    @Size(min = 1, max = 256, message = "Please provide a valid name between 1 and 256 characters")
     private String name;
+
+    @NotNull(message = "Phone must be not null")
+    @Pattern(regexp = "^(\\+7|([78]))(\\d{10})$",
+            message = "Phone must be eleven digit and starts with 7 or 8, valid examples 78005551133, 88129992277, +79112003333")
     private String phone;
+
+    @EqualsAndHashCode.Exclude
     private LocalDateTime lastModified = LocalDateTime.now();
+
+    public PhoneRecord(String name, String phone) {
+        this.name = trimName(name);
+        this.phone = formatPhoneNumber(phone);
+    }
 }
