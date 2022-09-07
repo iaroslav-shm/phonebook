9190104157eb9b17a26c869c9006a948698f4419
Iaroslav Shmorgunov <yaroslav.shm@gmail.com>
2022-09-07 14:10:13 +0300

    Add beautiful Validation Exception output in JSON

--- src/main/java/org/shmorgunov/phonebook/web/PhoneBookController.java
+++ src/main/java/org/shmorgunov/phonebook/web/PhoneBookController.java
@@ -6,11 +6,11 @@ import org.shmorgunov.phonebook.domain.PhoneRecord;
 import org.shmorgunov.phonebook.service.PhoneBookService;
 import org.shmorgunov.phonebook.util.ApiConstants;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,7 +19,7 @@ import java.util.Optional;
 @RequiredArgsConstructor
 @RequestMapping(path = ApiConstants.RECORDS,
         produces = "application/json")
-@CrossOrigin(origins = "http://phonebook:8080")
+@CrossOrigin()
 public class PhoneBookController {
 
     private PhoneBookService phoneBookService;
@@ -45,12 +45,12 @@ public class PhoneBookController {
 
     @PostMapping(consumes = "application/json")
     @ResponseStatus(HttpStatus.CREATED)
-    public PhoneRecord postPhoneRecord(@RequestBody PhoneRecord phoneRecord) {
-        return phoneBookService.saveRecord(phoneRecord);
+    public PhoneRecord postPhoneRecord(@Valid @RequestBody PhoneRecord phoneRecord) {
+        return phoneBookService.addRecord(phoneRecord);
     }
 
     @PatchMapping(path = ApiConstants.RECORD_ID, consumes = "application/json")
-    public ResponseEntity<PhoneRecord> patchPhoneRecord(@PathVariable("id") Long id, @RequestBody PhoneRecord patch) {
+    public ResponseEntity<PhoneRecord> patchPhoneRecord(@PathVariable("id") Long id, @Valid @RequestBody PhoneRecord patch) {
         Optional<PhoneRecord> optionalPhoneRecord = phoneBookService.patchPhoneRecordById(id, patch);
         return optionalPhoneRecord
                 .map(phoneRecord -> new ResponseEntity<>(phoneRecord, HttpStatus.OK))
@@ -60,8 +60,6 @@ public class PhoneBookController {
     @DeleteMapping(ApiConstants.RECORD_ID)
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteOrder(@PathVariable("id") Long orderId) {
-        try {
-            phoneBookService.deletePhoneRecordById(orderId);
-        } catch (EmptyResultDataAccessException e) {}
+        phoneBookService.deletePhoneRecordById(orderId);
     }
 }
